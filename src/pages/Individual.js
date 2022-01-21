7e8c780607ebf1482c2dcf8b2216c7d780b5b22e
rananischal15@gmail.com <rananischal15@gmail.com>
2022-01-21 13:58:36 +1100

    single page issue solved

--- src/pages/Individual.js
+++ src/pages/Individual.js
@@ -4,75 +4,66 @@ import "./css/individual.css";
 import { useParams, useRouteMatch } from "react-router-dom";
 
 const Individual = () => {
-  const { getSingleHero, product, products } = useContext(ProductContext);
-
-  let { targetId } = useParams();
-
   useEffect(() => {
     getSingleHero(targetId);
   }, []);
 
-  if (!product) {
-    return <h2>No Data Found</h2>;
-  }
+  const context = useContext(ProductContext);
+  const { getSingleHero, product } = context;
 
-  const {
-    id,
-    name,
-    appearance,
-    className,
-    biography,
-    connections,
-    image,
-    powerstats,
-    work,
-  } = product;
+  let { targetId } = useParams();
 
-  return (
-    <div className="single-hero-container">
-      <section className="hero-img">
-        <img src={image.url} alt="hero snap" />
-      </section>
+  const Showcase = () => {
+    return (
+      <div className="single-hero-container">
+        <section className="hero-img">
+          <img src={product.image.url} alt="hero snap" />
+        </section>
 
-      <section className="hero-details">
-        <h1 style={{ textAlign: "center", fontWeight: "bold" }}>{name}</h1>
+        <section className="hero-details">
+          <h1 style={{ textAlign: "center", fontWeight: "bold" }}>
+            {product.name}
+          </h1>
 
-        <div className="fulldetails-container">
-          <h1>BIOGRAPHY :</h1>
-          <h4>FullName: {biography["full-name"]}</h4>
-          <h4>First Appearance: {biography["first-appearance"]}</h4>
-          <h4>Alignment: {biography.alignment}</h4>
-          <h4>Publisher: {biography.publisher}</h4>
-          <h4>
-            Aliases: [{" "}
-            {biography.aliases
-              .map((name) => {
-                return name;
-              })
-              .join(", ")}{" "}
-            ]
-          </h4>
-          <h4>Relatives: [ {connections.relatives} ]</h4>
-          <h4>Group: [ {connections["group-affiliation"]} ]</h4>
-        </div>
+          <div className="fulldetails-container">
+            <h1>BIOGRAPHY :</h1>
+            <h4>FullName: {product.biography["full-name"]}</h4>
+            <h4>First Appearance: {product.biography["first-appearance"]}</h4>
+            <h4>Alignment: {product.biography.alignment}</h4>
+            <h4>Publisher: {product.biography.publisher}</h4>
+            <h4>
+              Aliases: [{" "}
+              {product.biography.aliases
+                .map((name) => {
+                  return name;
+                })
+                .join(", ")}{" "}
+              ]
+            </h4>
+            <h4>Relatives: [ {product.connections.relatives} ]</h4>
+            <h4>Group: [ {product.connections["group-affiliation"]} ]</h4>
+          </div>
 
-        <div className="fulldetails-container">
-          <h1>POWERSTATS :</h1>
-          <h4>Combat: {powerstats.combat}</h4>
-          <h4>Durability: {powerstats.durability}</h4>
-          <h4>Intelligence: {powerstats.intelligence}</h4>
-          <h4>Power: {powerstats.power}</h4>
-          <h4>Speed: {powerstats.speed}</h4>
-          <h4>Strength: {powerstats.strength}</h4>
-        </div>
+          <div className="fulldetails-container">
+            <h1>POWERSTATS :</h1>
+            <h4>Combat: {product.powerstats.combat}</h4>
+            <h4>Durability: {product.powerstats.durability}</h4>
+            <h4>Intelligence: {product.powerstats.intelligence}</h4>
+            <h4>Power: {product.powerstats.power}</h4>
+            <h4>Speed: {product.powerstats.speed}</h4>
+            <h4>Strength: {product.powerstats.strength}</h4>
+          </div>
 
-        <div className="fulldetails-container">
-          <h1>Work :</h1>
-          <h4>Occupation: [ {work.occupation} ]</h4>
-        </div>
-      </section>
-    </div>
-  );
+          <div className="fulldetails-container">
+            <h1>Work :</h1>
+            <h4>Occupation: [ {product.work.occupation} ]</h4>
+          </div>
+        </section>
+      </div>
+    );
+  };
+
+  return <div>{product ? <Showcase /> : <p>Nothing to show</p>}</div>;
 };
 
 export default Individual;
